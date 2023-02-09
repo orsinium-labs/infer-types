7e1cdcf68afdc24f34ce3256d3fd213b1297257b
gram <git@orsinium.dev>
2023-02-09 10:23:44 +0100

    more cli options

--- infer_types/_cli.py
+++ infer_types/_cli.py
@@ -13,6 +13,9 @@ from ._inferno import Inferno
 @dataclass(frozen=True)
 class Config:
     format: bool
+    skip_tests: bool
+    skip_migrations: bool
+    dry: bool
     stream: TextIO
 
 
@@ -20,14 +23,19 @@ def generate_stubs(root: Path, config: Config) -> None:
     inferno = Inferno()
     for path in root.iterdir():
         if path.is_dir():
+            if config.skip_migrations and path.name == 'migrations':
+                continue
             generate_stubs(path, config)
             continue
         if path.suffix != '.py':
             continue
+        if config.skip_tests and path.name.startswith('test_'):
+            continue
         new_source = inferno.transform(path)
         if config.format:
             new_source = format_code(new_source)
-        path.write_text(new_source)
+        if not config.dry:
+            path.write_text(new_source)
         print(path, file=config.stream)
 
 
@@ -41,8 +49,26 @@ def main(argv: list[str], stream: TextIO) -> int:
         '--format', action='store_true',
         help='run available code formatters on the modified files',
     )
+    parser.add_argument(
+        '--skip-tests', action='store_true',
+        help='skip test files (starting with `test_`)',
+    )
+    parser.add_argument(
+        '--skip-migrations', action='store_true',
+        help='skip Django migration files',
+    )
+    parser.add_argument(
+        '--dry', action='store_true',
+        help='do not modify any files',
+    )
     args = parser.parse_args(argv)
-    config = Config(format=args.format, stream=stream)
+    config = Config(
+        format=args.format,
+        skip_tests=args.skip_tests,
+        skip_migrations=args.skip_migrations,
+        dry=args.dry,
+        stream=stream,
+    )
     generate_stubs(args.dir, config)
     return 0
 
